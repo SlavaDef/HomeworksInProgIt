1c3fd25354402b40034276f1a5a404f1a8d6eff4
SlavaDef <slavadef@gmail.com>
2023-11-24 00:05:54 +0200

    new com 7

---
+++ src/main/java/org/homework/thirtTasks/referenses/Functional2.java
@@ -0,0 +1,47 @@
+package org.homework.thirtTasks.referenses;
+
+// Напишіть програму, в якій є клас з закритим текстовим полем і два відкритих
+//методи: один дозволяє присвоїти значення полю, інший результатом повертає
+//значення цього поля. Створіть два функціональних інтерфейси, абстрактні методи
+//в яких відповідають описанню методів у класі. Створіть об’єкт класу. Для кожного
+//з інтерфейсів створіть інтерфейсну змінну і значенням присвойте посилання на
+//відповідний метод об’єкта
+
+class Functional2 {
+    public Functional2(String text) {
+        this.text = text;
+    }
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
+interface TextGetter {
+    String textGet();
+}
+
+interface TextSetter {
+    void texSet(String text);
+
+}
+
+class Tester {
+    public static void main(String[] args) {
+
+        Functional2 f = new Functional2("uno");
+        System.out.println("Object has been created!");
+        TextGetter textGetter = f::getText;
+        TextSetter textSetter = f::setText;
+        System.out.println(textGetter.textGet());
+        textSetter.texSet("kvanto");
+        System.out.println(textGetter.textGet());
+
+    }
+}
