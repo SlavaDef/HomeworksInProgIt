f2747c0afe65ded08c5966750684cde8b13fb8a3
SlavaDef <slavadef@gmail.com>
2023-12-08 18:24:01 +0200

    new com 22

--- src/main/java/org/homework/fourTasks/threads/ThreadFinder3.java
+++ src/main/java/org/homework/fourTasks/threads/ThreadFinder3.java
@@ -1,30 +1,39 @@
 package org.homework.fourTasks.threads;
 
-
 // Напишіть програму, в якій створюється двовимірний цілочисловий масив.
 //Програма знаходить в кожному рядку масиву елемент з найбільшим значенням.
 //Пошук елементу з найбільшим значенням в кожному рядку виконується окремим
 //потоком.
-class ThreadFinder2 extends Thread {
+public class ThreadFinder3 extends Thread{
 
     private int temp;
+
+    private int rows;
+    private int columns;
+    int[][] arr = new int[rows][columns];
+
 
     private final int[][] numbers = {{44, 32,  88, -2}, {123, 84, 99, -7}, {-66,  -234, -270, -1},
             {-72,  -12, -15, -6}, {44, 32, 12,  -2}, {44, 32,  88, -2}, { 32, 12, 88, -2}, {123, 84,  99, -7}};
 
 
-    public ThreadFinder2(String name, int temp) {
+    public ThreadFinder3(String name, int temp) {
         super(name);
         this.temp = temp;
         start();
 
     }
 
-    public int getNumbersLength() {
+    public int getNumbers() {
         return numbers.length;
     }
 
-    public ThreadFinder2() {
+    public ThreadFinder3() {
+    }
+
+    public int[][] createArray(int rows,int columns){
+
+        return new int[rows][columns];
     }
 
     @Override
@@ -47,11 +56,12 @@ class ThreadFinder2 extends Thread {
     }
 }
 
-class Test {
+class Test3 {
 
     public static void main(String[] args) {
 
-        int a = new ThreadFinder2().getNumbersLength();
+        int a = new ThreadFinder3().getNumbers();
+        System.out.println(a);
 
         for (int i = 0; i < a; i++) {
             new ThreadFinder2("", i);
@@ -59,4 +69,5 @@ class Test {
 
 
     }
+
 }
